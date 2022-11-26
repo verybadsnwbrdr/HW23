88b906be3a05c6698804e1564a6a4f56c45780bb
Anton Khrapov <froe054@mail.ru>
2022-11-26 14:43:35 +0700

    Initial Commit

---
+++ HW23/HW23App.swift
@@ -0,0 +1,17 @@
+//
+//  HW23App.swift
+//  HW23
+//
+//  Created by Anton on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
