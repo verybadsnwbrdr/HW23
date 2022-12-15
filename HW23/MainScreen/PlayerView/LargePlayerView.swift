5b2cfb95c080feb58a50d81754bdd5eec7055b60
Anton Khrapov <froe054@mail.ru>
2022-12-15 20:38:09 +0700

    save changes

--- HW23/MainScreen/PlayerView/LargePlayerView.swift
+++ HW23/MainScreen/PlayerView/LargePlayerView.swift
@@ -8,85 +8,97 @@
 import SwiftUI
 
 struct LargePlayerView: View {
-    @State var musicProgress = 0.0
-    
-    var body: some View {
-        ZStack {
-            Color.gray
-                .ignoresSafeArea()
-            
-            VStack(spacing: 30) {
-                Image("1")
-                    .resizable()
-                    .frame(width: 300, height: 300)
-                    .cornerRadius(20)
-                    .shadow(radius: 10)
-                
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Ssss")
-                            .bold()
-                        Text("Evgenia")
+          @State var musicProgress = 0.0
+          
+          var body: some View {
+                    ZStack {
+                              LinearGradient(colors: [.brown, .gray],
+                                             startPoint: .top,
+                                             endPoint: .bottom)
+                              .ignoresSafeArea()
+                              
+                              VStack(spacing: 30) {
+                                        Image("1")
+                                                  .resizable()
+                                                  .frame(width: 300, height: 300)
+                                                  .cornerRadius(20)
+                                                  .shadow(radius: 10)
+                                        
+                                        HStack {
+                                                  VStack(alignment: .leading) {
+                                                            Text("Ssss")
+                                                                      .bold()
+                                                            Text("Evgenia")
+                                                  }
+                                                  .font(.system(size: 24))
+                                                  
+                                                  Spacer()
+                                                  Button {
+                                                            
+                                                  } label: {
+                                                            ZStack {
+                                                                      Circle()
+                                                                                .frame(width: 35, height: 35)
+                                                                                .foregroundColor(.white)
+                                                                                .opacity(0.1)
+                                                                                .shadow(radius: 10)
+                                                                      Image(systemName: "ellipsis")
+                                                                                .resizable()
+                                                                                .frame(width: 22, height: 5)
+                                                                      
+                                                            }
+                                                            
+                                                  }
+                                        }
+                                        
+                                        Slider(value: $musicProgress)
+                                        
+                                        HStack(spacing: 80) {
+                                                  Button {
+                                                            
+                                                  } label: {
+                                                            Image(systemName: "backward.fill")
+                                                                      .resizable()
+                                                                      .frame(width: 50, height: 30)
+                                                  }
+                                                  
+                                                  Button {
+                                                            
+                                                  } label: {
+                                                            Image(systemName: "play.fill")
+                                                                      .resizable()
+                                                                      .frame(width: 45, height: 45)
+                                                  }
+                                                  
+                                                  Button {
+                                                            
+                                                  } label: {
+                                                            Image(systemName: "forward.fill")
+                                                                      .resizable()
+                                                                      .frame(width: 50, height: 30)
+                                                  }
+                                        }
+                                        .shadow(radius: 10)
+                                        
+                                        Slider(value: $musicProgress) {
+                                        } minimumValueLabel: {
+                                                  Image(systemName: "goforward.plus")
+                                        } maximumValueLabel: {
+                                                  Image(systemName: "goforward.plus")
+                                        }
+                                        
+                                        
+                                        
+                              }
+                              .padding([.leading, .trailing], 30)
+                              .foregroundColor(.white)
+                              
                     }
-                    .font(.system(size: 24))
-                    
-                    Spacer()
-                    Circle()
-                        .frame(width: 35)
-                        .overlay {
-                            HStack(spacing: 3) {
-                                Circle()
-                                    .frame(width: 5)
-                                Circle()
-                                    .frame(width: 5)
-                                Circle()
-                                    .frame(width: 5)
-                            }
-                            .foregroundColor(.white)
-                        }
-                        .foregroundColor(.gray)
-                        .shadow(radius: 4)
-                }
-                
-                Slider(value: $musicProgress)
-
-                HStack(spacing: 80) {
-                    Image(systemName: "backward.fill")
-                        .resizable()
-                        .frame(width: 50, height: 30)
-                    
-                    Image(systemName: "play.fill")
-                        .resizable()
-                        .frame(width: 45, height: 45)
-                    
-                    Image(systemName: "forward.fill")
-                        .resizable()
-                        .frame(width: 50, height: 30)
-                    
-                }
-                .shadow(radius: 10)
-                
-                Slider(value: $musicProgress) {
-                } minimumValueLabel: {
-                    Image(systemName: "goforward.plus")
-                } maximumValueLabel: {
-                    Image(systemName: "goforward.plus")
-                }
-                
-                
-                
-            }
-            .padding([.leading, .trailing], 30)
-            .foregroundColor(.white)
-            
-        }
-        //        .padding()
-        
-    }
+          }
 }
 
 struct LargePlayerView_Previews: PreviewProvider {
-    static var previews: some View {
-        LargePlayerView()
-    }
+          static var previews: some View {
+                    LargePlayerView()
+          }
 }
---
+++ HW23/MainScreen/PlayerView/SliderPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  SliderPlayerView.swift
+//  HW23
+//
+//  Created by Anton on 15.12.2022.
+//
+
+import SwiftUI
+
+struct SliderPlayerView: View {
+	var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
+}
+
+struct SliderPlayerView_Previews: PreviewProvider {
+	static var previews: some View {
+		SliderPlayerView()
+	}
+}
